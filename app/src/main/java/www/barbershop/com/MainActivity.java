eeeaba258eeb9252db3577a703da922670c7b910
Miguel Ochoa <ochoamiguel1409@gmail.com>
2024-04-30 13:43:29 -0500

    Initial commit

---
+++ app/src/main/java/www/barbershop/com/MainActivity.java
@@ -0,0 +1,44 @@
+package www.barbershop.com;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.hoja2), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+public void irAIngresar(View view){
+        Intent i = new Intent(this, hoja2.class);
+        startActivity(i);
+
+}
+
+public void irARegistrate(View view){
+        Intent i = new Intent(this, Registro.class);
+        startActivity(i);
+}
+
+
+}
+
+
+
+
---
+++ app/src/main/java/www/barbershop/com/hoja2.java
@@ -0,0 +1,36 @@
+package www.barbershop.com;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class hoja2 extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_hoja2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.hoja2), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+
+    public void irAIniciarSesion(View view){
+        Intent i = new Intent(this, Cliente.class);
+        startActivity(i);
+    }
+
+
+}
